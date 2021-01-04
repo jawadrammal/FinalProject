50d7689563623daf8a8ddb5697042ac93c82e123
jawadrammal <49551384+jawadrammal@users.noreply.github.com>
2021-01-04 12:01:26 +0200

    updates - weight check (WeightThreshold) ,savingsol in cargolist page ,run in portrait , change backbutton ,...

--- app/src/main/java/com/example/myapplication/CargoInContainer.java
+++ app/src/main/java/com/example/myapplication/CargoInContainer.java
@@ -155,6 +155,7 @@ public class CargoInContainer extends AppCompatActivity {
                      }
                 }
             }
+            saveSolution();
         }
        View container = ((View) findViewById(R.id.Container));
         CargoTablePage.containerX = MainActivity.MainInfo.ContainerStartX*MainActivity.MainInfo.screenWidth;
